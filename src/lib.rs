e9b60721104361a2ace0d592551050f35a7d8178
Sebastian Thiel <byronimo@gmail.com>
2015-05-06 16:42:55 +0200

    feat(lexer): string value parsing
    Including test

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,11 @@
 pub struct Lexer<I> 
                 where I: Iterator<Item=char> {
     chars: I,
-    cursor: u64,
+    prev_end: u64,
 }
 
 #[derive(Debug, PartialEq)]
 pub enum TokenType {
-    /// All whitespace
-    WhiteSpace,
-
     /// `{`
     CurlyOpen,
     /// `}`
@@ -47,9 +44,9 @@ pub enum TokenType {
 /// It is an exclusive range.
 #[derive(Debug, PartialEq)]
 pub struct Span {
-    /// Index of the first the character
+    /// Index of the first the unicode character
     pub first: u64,
-    /// Index one past the last character
+    /// Index one past the last unicode character
     pub end: u64,
 }
 
@@ -68,7 +65,7 @@ impl<I> Lexer<I> where I: Iterator<Item=char> {
     pub fn new(chars: I) -> Lexer<I> {
         Lexer {
             chars: chars,
-            cursor: 0,
+            prev_end: 0,
         }
     }
 }
@@ -79,6 +76,85 @@ impl<I> Iterator for Lexer<I>
 
     /// Lex the underlying character stream to generate tokens
     fn next(&mut self) -> Option<Token> {
-        None
+        let mut t: TokenType = TokenType::Invalid;
+        let mut lcid: u64 = 0;
+
+        let mut in_str = false;     // true if we are lexing a string
+        let mut ign_next = false;   // ignore next special character meaning
+
+        let mut first = 0;
+        let prev_end = self.prev_end;
+
+        for c in self.chars.by_ref() {
+            lcid += 1;
+
+            let mut set_cursor = || {
+                first = prev_end + lcid - 1;
+            };
+
+            match c {
+                '\\' => {
+                    if in_str {
+                        ign_next = true;
+                        continue; // this is allowed only here !
+                    } else {
+                        // invalid
+                        assert_eq!(t, TokenType::Invalid);
+                        set_cursor();
+                        break
+                    }
+                }
+                '{'
+                |'}'
+                |'['
+                |']'
+                |':'
+                |',' => {
+                    if !in_str {
+                        set_cursor();
+                        t = match c {
+                            '{' => TokenType::CurlyOpen,
+                            '}' => TokenType::CurlyClose,
+                            '[' => TokenType::BracketOpen,
+                            ']' => TokenType::BracketClose,
+                            ':' => TokenType::Colon,
+                            ',' => TokenType::Comma,
+                            _ => unreachable!(),
+                        };
+                        break;
+                    }
+                },
+                '"' => {
+                    if !ign_next {
+                        if in_str {
+                            break;
+                        } else {
+                            in_str = true;
+                            t = TokenType::StringValue;
+                            set_cursor();
+                        }
+                    }
+                },
+                _ => {
+                    // Everything here is considered whitespace, which is skipped
+                },
+            }// end single character match
+
+            // NOTE: MUST NOT CONTINUE !
+            ign_next = false;
+        }// end for each character
+
+        if lcid == 0 {
+            None
+        } else {
+            self.prev_end = self.prev_end + lcid;
+            Some(Token {
+                kind: t,
+                span: Span {
+                    first: first,
+                    end: self.prev_end
+                }
+            })
+        }
     }
 }
--- tests/lexer.rs
+++ tests/lexer.rs
@@ -7,9 +7,21 @@ fn unicode() {
     let src = r#"{ "face": "😂" }"#;
     let mut it = Lexer::new(src.chars());
 
-    assert_eq!(it.next(), Some(Token { kind: TokenType::BracketLeft, 
+    assert_eq!(it.next(), Some(Token { kind: TokenType::CurlyOpen, 
                                        span: Span { first: 0,
                                                     end:  1 } }));
+    assert_eq!(it.next(), Some(Token { kind: TokenType::StringValue, 
+                                       span: Span { first: 2,
+                                                    end:  8 } }));
+    assert_eq!(it.next(), Some(Token { kind: TokenType::Colon, 
+                                       span: Span { first: 8,
+                                                    end:  9 } }));
+    assert_eq!(it.next(), Some(Token { kind: TokenType::StringValue, 
+                                       span: Span { first: 10,
+                                                    end:  13 } }));
+    assert_eq!(it.next(), Some(Token { kind: TokenType::CurlyClose, 
+                                       span: Span { first: 14,
+                                                    end:  15 } }));
 }
 
 
